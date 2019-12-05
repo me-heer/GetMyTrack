6133774fddc668113211c7422672fee93619f6c3
Mihir Joshi <mihir67mj@gmail.com>
2019-12-05 13:02:59 +0530

    Initial Setup

---
+++ src/main/java/getmytrack/Authorization.java
@@ -0,0 +1,58 @@
+package getmytrack;
+/**
+ * Hello world!
+ */
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+
+import java.io.IOException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+public class Authorization {
+  private static final String clientId = "a0ac3f1c6676404890e169170cfe4b0a";
+  private static final String clientSecret = "785bee0586374ae88e631681da8ef9e6";
+
+  private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+          .setClientId(clientId)
+          .setClientSecret(clientSecret)
+          .build();
+  private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
+          .build();
+
+  public static void clientCredentials_Sync() {
+    try {
+      final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+
+      // Set access token for further "spotifyApi" object usage
+      spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+      System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+    } catch (IOException | SpotifyWebApiException e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
+
+  public static void clientCredentials_Async() {
+    try {
+      final CompletableFuture<ClientCredentials> clientCredentialsFuture = clientCredentialsRequest.executeAsync();
+
+      // Thread free to do other tasks...
+
+      // Example Only. Never block in production code.
+      final ClientCredentials clientCredentials = clientCredentialsFuture.join();
+
+      // Set access token for further "spotifyApi" object usage
+      spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+      System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+    } catch (CompletionException e) {
+      System.out.println("Error: " + e.getCause().getMessage());
+    } catch (CancellationException e) {
+      System.out.println("Async operation cancelled.");
+    }
+  }
+}
